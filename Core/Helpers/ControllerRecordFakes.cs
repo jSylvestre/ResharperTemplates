29dfbe8a4324a870123d3ee9829c58f3424b3d79
Jason Sylvestre <jsylvestre@ucdavis.edu>
2011-04-12 10:34:58 -0700

    Added RoutTestingExtensions (to Ignore Parameters)
    Edited ControllerRecordFakes to just have an example because the other part was moved to UCDArch.Testing.Fakes
    Added PowerPoint for writting Unit Tests

--- Core/Helpers/ControllerRecordFakes.cs
+++ Core/Helpers/ControllerRecordFakes.cs
@@ -2,66 +2,17 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
-using Gramps.Core.Domain;
-using Rhino.Mocks;
-using UCDArch.Core.DomainModel;
-using UCDArch.Core.PersistanceSupport;
-using UCDArch.Testing;
+//using YourProject.Core.Domain;
+using UCDArch.Testing.Fakes;
 
-namespace Gramps.Tests.Core.Helpers
+namespace YourProject.Tests.Core.Helpers
 {
-    public abstract class ControllerRecordFakes<T> where T : DomainObject
-    {
-        public void Records(int count, IRepository<T> repository)
-        {
-            var records = new List<T>();
-            Records(count, repository, records);
-        }
-
-
-        public void Records(int count, IRepository<T> repository, List<T> specificRecords)
-        {
-            var records = new List<T>();
-            var specificRecordsCount = 0;
-            if (specificRecords != null)
-            {
-                specificRecordsCount = specificRecords.Count;
-                for (int i = 0; i < specificRecordsCount; i++)
-                {
-                    records.Add(specificRecords[i]);
-                }
-            }
-
-            for (int i = 0; i < count; i++)
-            {
-                records.Add(CreateValid(i + specificRecordsCount + 1));
-            }
-
-            var totalCount = records.Count;
-            for (int i = 0; i < totalCount; i++)
-            {
-                records[i].SetIdTo(i + 1);
-                int i1 = i;
-                repository
-                    .Expect(a => a.GetNullableById(i1 + 1))
-                    .Return(records[i])
-                    .Repeat
-                    .Any();
-            }
-            repository.Expect(a => a.GetNullableById(totalCount + 1)).Return(null).Repeat.Any();
-            repository.Expect(a => a.Queryable).Return(records.AsQueryable()).Repeat.Any();
-            repository.Expect(a => a.GetAll()).Return(records).Repeat.Any();
-        }
-
-        protected abstract T CreateValid(int i);
-    }
-
-    public class FakeExamples : ControllerRecordFakes<Example>
-    {
-        protected override Example CreateValid(int i)
-        {
-            return CreateValidEntities.Example(i);
-        }
-    }
-
+    //Example
+    //public class FakeAnswers : ControllerRecordFakes<Answer>
+    //{
+    //    protected override Answer CreateValid(int i)
+    //    {
+    //        return CreateValidEntities.Answer(i);
+    //    }
+    //}
 }
